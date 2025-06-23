15273a35300171fb6b818a73f64f4cf306e415e3
Arturo Espana <arturo.espana04@gmail.com>
2025-06-23 14:16:18 -0400

    delete files

--- espana/testingfile.R
+++ espana/testingfile.R
@@ -1,13 +1,14 @@
-#comment testing
-
+# we want to do analysis on primary care physicians, uninsured rate, race demographics 
 library(tidyverse)
 library(readxl)
-national_data <- read_csv("analytic_data2025_v2.csv")
-trend_data <- read_csv("chr_trends_csv_2025.csv")
-ranking_data <- read_excel("2025 County Health Rankings Data - v3.xlsx")
+national_data <- read_csv("data/analytic_data2025_v2.csv")
+trend_data <- read_csv("data/chr_trends_csv_2025.csv")
 view(national_data)
 view(trend_data)
 
 colnames(national_data)
 
 
+filter_national_data <- national_data |>
+  select(`State FIPS Code`, `County FIPS Code`, `5-digit FIPS Code`, `State Abbreviation`, `Name`, `Release Year`, `Premature Death raw value`, `Premature Death numerator`, `Premature Death denominator`)
+filter_national_data
