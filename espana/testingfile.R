ccf991f3885f833bb1e026b4638ba580ad5badbe
Arturo Espana <arturo.espana04@gmail.com>
2025-06-23 13:52:32 -0400

    hello

--- espana/testingfile.R
+++ espana/testingfile.R
@@ -1,1 +1,13 @@
 #comment testing
+
+library(tidyverse)
+library(readxl)
+national_data <- read_csv("analytic_data2025_v2.csv")
+trend_data <- read_csv("chr_trends_csv_2025.csv")
+ranking_data <- read_excel("2025 County Health Rankings Data - v3.xlsx")
+view(national_data)
+view(trend_data)
+
+colnames(national_data)
+
+
