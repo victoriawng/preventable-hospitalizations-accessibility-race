ee03e21d81772412a239b39de027753b361fe80b
Victoria Nguyen <victoriang893@gmail.com>
2025-06-24 14:14:24 -0400

    starting with missing values

--- Nguyen/EDA.R
+++ Nguyen/EDA.R
@@ -1,9 +1,5 @@
-# hello this is Victoria's EDA
-
 # understand healthcare access
 # preventable hospitalizations
-
-# 
 
 source("EDA_import.R")
 library(tidyverse)
@@ -12,11 +8,47 @@ view(national_data)
 view(trend_data)
 view(data_dict_2025)
 
-
 dim(national_data) #3205  796
 dim(trend_data) #745088     15
 dim(data_dict_2025) #796   3
 
-# install.packages("visdat")
+
+# missing values ---------------------------------------------------------------
 library(visdat)
-# vis_miss(covid_hospitalizations, warn_large_data = FALSE) # raw df # NA 14.5% missing
+# install.packages("visdat")
+vis_miss(national_data, warn_large_data = FALSE) +
+  #  hides the column names
+  theme(axis.text.x = element_blank())
+  # removes the little tick marks too
+        # ,axis.ticks.x = element_blank())
+
+# ---
+# https://www.geeksforgeeks.org/r-language/how-to-find-and-count-missing-values-in-r-dataframe/
+# Dec 2023
+
+# finds the location of missing values
+# indice NOT row or column
+# which(is.na(national_data))
+
+# finds the count of missing values 
+# sum(is.na(national_data))
+
+# ouputs length, class, and mode
+# summary(national_data[25]) # change number to change column summary
+
+
+# ---
+# https://www.geeksforgeeks.org/r-language/handling-missing-values-in-r-programming/
+# April 2025
+
+# display the number of NA values in each column
+class(colSums(is.na(national_data)))
+class(national_data)
+
+
+class(is.na(national_data))
+
+
+vis_miss(national_data, warn_large_data = FALSE)
+
+
