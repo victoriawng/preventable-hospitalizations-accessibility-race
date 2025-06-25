8c534a3d9bbdf6255b9a0b250cbb8ce18f364dfa
Payson Johanson <pwjohanson55@gmail.com>
2025-06-25 13:30:53 -0400

    Map data, numerical, categorical columns

--- Johanson/eda_health_data_clean.R
+++ Johanson/eda_health_data_clean.R
@@ -1,133 +1,208 @@
 source("EDA_import.R")
-view(national_data)
-
-national_data_dt <- as.data.table(national_data)
-
-trend_data_dt <- as.data.table(trend_data)
-
-national_raw <- national_data |>
-  select(`State FIPS Code`, `State Abbreviation`, Name, `County FIPS Code`, `Release Year`, contains("raw value"))
 
 
+#Run at start
+library(tidyverse)
+library(naniar)
+library(janitor)
+library(VIM)
+library(stats)
 
-clean_national_subset <- national_data |> 
+national_data <- read_csv("data/analytic_data2025_v2.csv")
+
+#Clean names
+clean_names_national_data <- national_data |> 
+  clean_names()
+
+#Subset variables of interest
+clean_names_national_data_subset <- clean_names_national_data |> 
   select(
-    `State FIPS Code`,
-    `State Abbreviation`,
-    Name,
-    `County FIPS Code`,
-    `Flu Vaccinations raw value`,
-    `Flu Vaccinations (AIAN)`,
-    `Flu Vaccinations (Asian/Pacific Islander)`,
-    `Flu Vaccinations (Black)`,
-    `Flu Vaccinations (Hispanic)`,
-    `Flu Vaccinations (White)`,
-    `Primary Care Physicians raw value`,
-    `Ratio of population to primary care physicians.`,
-    `Mental Health Providers raw value`,
-    `Ratio of population to mental health providers.`,
-    `Dentists raw value`,
-    `Ratio of population to dentists.`,
-    `Preventable Hospital Stays raw value`,
-    `Preventable Hospital Stays (AIAN)`,
-    `Preventable Hospital Stays (Asian/Pacific Islander)`,
-    `Preventable Hospital Stays (Black)`,
-    `Preventable Hospital Stays (Hispanic)`,
-    `Preventable Hospital Stays (White)`,
-    `Mammography Screening raw value`,
-    `Mammography Screening (AIAN)`,
-    `Mammography Screening (Asian/Pacific Islander)`,
-    `Mammography Screening (Black)`,
-    `Mammography Screening (Hispanic)`,
-    `Mammography Screening (White)`,
-    `Uninsured raw value`,
-    `Uninsured Adults raw value`,
-    `Severe Housing Problems raw value`,
-    `Percentage of households with overcrowding`,
-    `Percentage of households with high housing costs`,
-    `Broadband Access raw value`,
-    `Library Access raw value`,
-    `Some College raw value`,
-    `High School Completion raw value`,
-    `Unemployment raw value`,
-    `Income Inequality raw value`,
-    `Children in Poverty raw value`,
-    `Children in Poverty (AIAN)`,
-    `Children in Poverty (White)`,
-    `Children in Poverty (Black)`,
-    `Children in Poverty (Hispanic)`,
-    `Children in Poverty (Asian/Pacific Islander)`,
-    `Child Care Cost Burden raw value`,
-    `Child Care Centers raw value`,
-    `Frequent Physical Distress raw value`,
-    `Frequent Mental Distress raw value`,
-    `Limited Access to Healthy Foods raw value`,
-    `Food Insecurity raw value`,
-    `Insufficient Sleep raw value`,
-    `Teen Births raw value`,
-    `Teen Births (AIAN)`,
-    `Teen Births (Black)`,
-    `Teen Births (NHOPI)`,
-    `Teen Births (White)`,
-    `Teen Births (Asian)`,
-    `Teen Births (Hispanic)`,
-    `Teen Births (Two or more races)`,
-    `Excessive Drinking raw value`,
-    `Adult Smoking raw value`,
-    `Adult Obesity raw value`,
-    `Physical Inactivity raw value`,
-    `Uninsured Children raw value`,
-    `Other Primary Care Providers raw value`,
-    `Ratio of population to primary care providers other than physicians.`,
-    `Traffic Volume raw value`,
-    `Homeownership raw value`,
-    `Severe Housing Cost Burden raw value`,
-    `Access to Parks raw value`,
-    `Census Participation raw value`,
-    `High School Graduation raw value`,
-    `Reading Scores raw value`,
-    `Reading Scores (AIAN)`,
-    `Reading Scores (White)`,
-    `Reading Scores (Black)`,
-    `Reading Scores (Hispanic)`,
-    `Reading Scores (Asian/Pacific Islander)`,
-    `Math Scores raw value`,
-    `Math Scores (AIAN)`,
-    `Math Scores (Black)`,
-    `Math Scores (Asian/Pacific Islander)`,
-    `Math Scores (Hispanic)`,
-    `Math Scores (White)`,
-    `School Segregation raw value`,
-    `School Funding Adequacy raw value`,
-    `Children Eligible for Free or Reduced Price Lunch raw value`,
-    `Median Household Income raw value`,
-    `Median Household Income (AIAN)`,
-    `Median Household Income (Black)`,
-    `Median Household Income (White)`,
-    `Median household income (Asian)`,
-    `Median Household Income (Hispanic)`,
-    `Living Wage raw value`,
-    `Residential Segregation - Black/White raw value`,
-    `Disconnected Youth raw value`,
-    `Lack of Social and Emotional Support raw value`,
-    `Population raw value`,
-    `Children in Single-Parent Households raw value`)
+    state_fips_code,
+    state_abbreviation,
+    name,
+    county_fips_code,
+    flu_vaccinations_raw_value,
+    flu_vaccinations_aian,
+    flu_vaccinations_asian_pacific_islander,
+    flu_vaccinations_black,
+    flu_vaccinations_hispanic,
+    flu_vaccinations_white,
+    primary_care_physicians_raw_value,
+    ratio_of_population_to_primary_care_physicians,
+    mental_health_providers_raw_value,
+    ratio_of_population_to_mental_health_providers,
+    dentists_raw_value,
+    ratio_of_population_to_dentists,
+    preventable_hospital_stays_raw_value,
+    preventable_hospital_stays_aian,
+    preventable_hospital_stays_asian_pacific_islander,
+    preventable_hospital_stays_black,
+    preventable_hospital_stays_hispanic,
+    preventable_hospital_stays_white,
+    mammography_screening_raw_value,
+    mammography_screening_aian,
+    mammography_screening_asian_pacific_islander,
+    mammography_screening_black,
+    mammography_screening_hispanic,
+    mammography_screening_white,
+    uninsured_raw_value,
+    uninsured_adults_raw_value,
+    severe_housing_problems_raw_value,
+    percentage_of_households_with_overcrowding,
+    percentage_of_households_with_high_housing_costs,
+    broadband_access_raw_value,
+    library_access_raw_value,
+    some_college_raw_value,
+    high_school_completion_raw_value,
+    children_in_poverty_raw_value,
+    children_in_poverty_aian,
+    children_in_poverty_asian_pacific_islander,
+    children_in_poverty_black,
+    children_in_poverty_hispanic,
+    children_in_poverty_white,
+    child_care_cost_burden_raw_value,
+    child_care_centers_raw_value,
+    frequent_physical_distress_raw_value,
+    frequent_mental_distress_raw_value,
+    limited_access_to_healthy_foods_raw_value,
+    food_insecurity_raw_value,
+    insufficient_sleep_raw_value,
+    teen_births_raw_value,
+    teen_births_aian,
+    teen_births_asian,
+    teen_births_black,
+    teen_births_hispanic,
+    teen_births_white,
+    teen_births_nhopi,
+    teen_births_two_or_more_races,
+    excessive_drinking_raw_value,
+    adult_smoking_raw_value,
+    adult_obesity_raw_value,
+    physical_inactivity_raw_value,
+    uninsured_children_raw_value,
+    other_primary_care_providers_raw_value,
+    ratio_of_population_to_primary_care_providers_other_than_physicians,
+    traffic_volume_raw_value,
+    homeownership_raw_value,
+    severe_housing_cost_burden_raw_value,
+    severe_housing_problems_raw_value,
+    access_to_parks_raw_value,
+    census_participation_raw_value,
+    high_school_graduation_raw_value,
+    reading_scores_raw_value,
+    reading_scores_aian,
+    reading_scores_asian_pacific_islander,
+    reading_scores_black,
+    reading_scores_hispanic,
+    reading_scores_white,
+    math_scores_raw_value,
+    math_scores_aian,
+    math_scores_asian_pacific_islander,
+    math_scores_black,
+    math_scores_hispanic,
+    math_scores_white,
+    living_wage_raw_value,
+    residential_segregation_black_white_raw_value,
+    disconnected_youth_raw_value,
+    lack_of_social_and_emotional_support_raw_value,
+    population_raw_value,
+    children_in_single_parent_households_raw_value,
+    percent_below_18_years_of_age_raw_value,
+    percent_65_and_older_raw_value,
+    percent_female_raw_value,
+    percent_american_indian_or_alaska_native_raw_value,
+    percent_asian_raw_value,
+    percent_hispanic_raw_value,
+    percent_native_hawaiian_or_other_pacific_islander_raw_value,
+    percent_non_hispanic_black_raw_value,
+    percent_non_hispanic_white_raw_value,
+    percent_disability_functional_limitations_raw_value,
+    percent_not_proficient_in_english_raw_value,
+    percent_rural_raw_value,
+  )
 
-clean_national_subset <- clean_national_subset |> 
-  filter(`State FIPS Code` != 'statecode')
+#Change necessary columns to numeric
+char_cols <- c("state_abbreviation", "name" )
 
+cols_to_convert <- setdiff(names(clean_names_national_subset_counties), char_cols)
 
-library(stringr)
+clean_names_national_subset_counties[cols_to_convert] <- lapply(
+  clean_names_national_subset_counties[cols_to_convert],
+  function(x) as.numeric(as.character(x))
+)
 
-
-
-non_county_names <- clean_national_subset |> 
-  filter(!str_detect(Name, "County")) |> 
-  select(Name)  
+clean_names_national_subset_counties[is.na(clean_names_national_subset_counties)] <- 0
 
   
 #Data set excluding US and States
-clean_national_subset_counties <- clean_national_subset |> 
-  filter(str_detect(Name, "County|Municipality|Census|Borough|Region|District|Parish|city"))
+clean_names_national_subset_counties <- clean_names_national_data_subset |> 
+  filter(str_detect(name, "County|Municipality|Census|Borough|Region|District|Parish|city"))
+
+
+# Get variable (column) names
+vars <- colnames(clean_names_national_subset_counties)
+
+# Split into 4 subsets
+var_groups <- split(vars, cut(seq_along(vars), 4, labels = FALSE))
+
+
+for (i in 1:4) {
+  
+  # Subset the data frame by variable group
+  data_subset <- clean_names_national_subset_counties[, var_groups[[i]]]
+  
+  # Plot missing values
+  print(gg_miss_var(data_subset) + ggtitle(paste("Missingness - Group", i)))
+}
+
+
+#Scatter plot variable against preventable hospital stays
+clean_names_national_subset_counties |>
+  ggplot(aes(x = reading_scores_raw_value,
+             y = preventable_hospital_stays_raw_value + 1)) + 
+  geom_point(alpha = 0.3, size = 1) +
+  scale_y_log10() +
+  labs(y = "Preventable Hospital Stays (log scale)")
+
+#Correlation between two variables
+with(clean_names_national_subset_counties, 
+     cor(reading_scores_raw_value,
+         preventable_hospital_stays_raw_value))
+
+#Map Stuff
+options(tigris_use_cache = TRUE)
+
+# Get shapefile for all US counties
+counties_sf <- counties(cb = TRUE, year = 2023, class = "sf")
+
+library(dplyr)
+library(stringr)
+
+clean_names_national_subset_counties <- clean_names_national_subset_counties |> 
+  mutate(
+    state_fips_code = str_pad(state_fips_code, 2, pad = "0"),
+    county_fips_code = str_pad(county_fips_code, 3, pad = "0"),
+    fips = paste0(state_fips_code, county_fips_code)
+  )
+
+map_data <- counties_sf |> 
+  left_join(clean_names_national_subset_counties, by = c("GEOID" = "fips"))
+
+ggplot(map_data) +
+  geom_sf(aes(fill = percent_hispanic_raw_value), color = NA) +
+  scale_fill_viridis(option = "magma", na.value = "grey90") +
+  theme_minimal() +
+  labs(title = "County-Level Map",
+       fill = "My Variable")
+
+ggplot(map_data) +
+  geom_sf(aes(fill = preventable_hospital_stays_raw_value), color = NA) +
+  coord_sf(xlim = c(-125, -66), ylim = c(24, 50)) +  # Approximate bounds of the lower 48
+  scale_fill_viridis_c(option = "magma", na.value = "grey90") +
+  theme_minimal()
+
+
+
+
 
 
