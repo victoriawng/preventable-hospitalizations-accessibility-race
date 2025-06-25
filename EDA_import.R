9379059e11ae61746861c8f60ef444b0700fbcfb
Victoria Nguyen <victoriang893@gmail.com>
2025-06-24 23:08:16 -0400

    SAS instead of CSV, report template added and whatever RDataTmp is

--- EDA_import.R
+++ EDA_import.R
@@ -1,11 +1,14 @@
 # importing data
 
-# https://www.countyhealthrankings.org/health-data/methodology-and-sources/data-documentation
+# data sets and dictionaries: https://www.countyhealthrankings.org/health-data/methodology-and-sources/data-documentation
 # install.packages("readxl")
-library(tidyverse)
+# install.packages("haven") 
+
 library(readxl)
-national_data <- read_csv("data/analytic_data2025_v2.csv")
-trend_data <- read_csv("data/chr_trends_csv_2025.csv")
+library(haven)
+# idk what a sas7bdat is but that's a later problem
+national_data = read_sas("data/analytic_data2025_v2.sas7bdat") 
+trend_data = read_sas("data/chr_trends_sas_2025.sas7bdat")
 data_dict_2025 = read_excel("data/DataDictionary_2025.xlsx")
 
 
--- Nguyen/EDA.R
+++ Nguyen/EDA.R
@@ -2,7 +2,17 @@
 # preventable hospitalizations
 
 source("EDA_import.R")
+
+# install.packages("visdat")
 library(tidyverse)
+library(skimr)
+library(visdat)
+
+# remember you can almost always use view() to see the output better
+# ------------------------------------------------------------------------------
+# exploring/understanding the data
+# -----------------------
+# understanding data set
 
 view(national_data)
 view(trend_data)
@@ -12,10 +22,46 @@ dim(national_data) #3205  796
 dim(trend_data) #745088     15
 dim(data_dict_2025) #796   3
 
+class(national_data) # "spec_tbl_df" "tbl_df"      "tbl"         "data.frame" 
+class(trend_data) # "spec_tbl_df" "tbl_df"      "tbl"         "data.frame" 
+class(data_dict_2025) # "tbl_df"     "tbl"        "data.frame"
+
+# class(data.frame())          # "data.frame"
+# class(tibble())              # "tbl_df" "tbl" "data.frame"
+# class(data.table())          # "data.table" "data.frame"
+# class(group_by(tibble(), x)) # "grouped_df" "tbl_df" "tbl" "data.frame"
+
+# ways to show class or data types of each column
+# https://www.geeksforgeeks.org/check-data-type-of-each-dataframe-column-in-r/
+str(national_data) # 
+sapply(national_data, class) 
+# notice how we will prob need to change chr to corresponding data type (exp: chr to int in order for us to plot)
+# it is all chr bc we read it in that way
+# if we wanted to, we can find if there's a way to import data so that the data type pertains or doesnt change
+
+class(national_data$)
+
+head(national_data)
+
+# -----------------------
+# descriptive statistics
+# do by columns or consider different data types
+
+# int is quantative 
+# fct is categorical 
+
+# for quantitative variables
+# summary()
+summary()
+
+# for categorical variables 
+# table()
+
+
+
 
 # missing values ---------------------------------------------------------------
-library(visdat)
-# install.packages("visdat")
+# library(visdat)
 vis_miss(national_data, warn_large_data = FALSE) +
   #  hides the column names
   theme(axis.text.x = element_blank())
@@ -42,13 +88,48 @@ vis_miss(national_data, warn_large_data = FALSE) +
 # April 2025
 
 # display the number of NA values in each column
+colSums(is.na(national_data))
+
+# na function
+get_na_proportion <- function(df) {
+  na_prop <- colSums(is.na(df)) / nrow(df)
+  result <- data.frame(
+    Column = names(na_prop),
+    ProportionMissing = as.vector(na_prop),
+    row.names = NULL
+  )
+  return(result)
+}
+
+view(get_na_proportion(national_data))
+
+view(get_na_proportion(national_data) |>
+  filter(ProportionMissing < 0.6))
+
+view(get_na_proportion(national_data) |>
+       filter(ProportionMissing > 0.6))
+
+
+
+df_with_60_na <- df_cleaned_column[rowMeans(is.na(df_cleaned_column)) > .6, ]
+# view(df_with_60_na)
+dim(df_with_60_na) #7035
+# view(count_NA(df_with_60_na))
+
+
 class(colSums(is.na(national_data)))
+
+
 class(national_data)
 
 
 class(is.na(national_data))
 
 
-vis_miss(national_data, warn_large_data = FALSE)
+get_na_proportion <- function(df) {
+  na_prop <- colSums(is.na(df)) / nrow(df)
+  data.frame(Column = names(na_prop), ProportionMissing = na_prop)
+}
+
 
 
