998e8571bb878cdba35fcbf7144e5c9e0efef4c4
Victoria Nguyen <victoriang893@gmail.com>
2025-06-26 13:19:47 -0400

    understanding Payson's, update my EDA

--- Nguyen/running_teams_code.R
+++ Nguyen/running_teams_code.R
@@ -1,6 +1,15 @@
-# install.packages("janitor")
-# install.packages("VIM")
+(.packages())
 
-national_data
 
-clean_names_national_data
+
+
+
+
+# Payson's eda_health_data_clean.R --------------------------------------------
+
+# Subset variables of interest
+view(clean_names_national_data_subset)
+
+# Only counties, muicipliacy, borough, parish, etc
+view(clean_names_national_subset_counties)
+
