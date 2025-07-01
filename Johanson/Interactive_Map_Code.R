f42e8bbba843f3f4d2280d3421f46601c76ad778
Payson Johanson <pwjohanson55@gmail.com>
2025-07-01 13:46:05 -0400

    data ammendments

--- Johanson/Interactive_Map_Code.R
+++ Johanson/Interactive_Map_Code.R
@@ -20,11 +20,6 @@ clean_names_national_data_subset <- clean_names_national_data |>
     name,
     county_fips_code,
     flu_vaccinations_raw_value,
-    flu_vaccinations_aian,
-    flu_vaccinations_asian_pacific_islander,
-    flu_vaccinations_black,
-    flu_vaccinations_hispanic,
-    flu_vaccinations_white,
     primary_care_physicians_raw_value,
     ratio_of_population_to_primary_care_physicians,
     mental_health_providers_raw_value,
@@ -32,17 +27,7 @@ clean_names_national_data_subset <- clean_names_national_data |>
     dentists_raw_value,
     ratio_of_population_to_dentists,
     preventable_hospital_stays_raw_value,
-    preventable_hospital_stays_aian,
-    preventable_hospital_stays_asian_pacific_islander,
-    preventable_hospital_stays_black,
-    preventable_hospital_stays_hispanic,
-    preventable_hospital_stays_white,
     mammography_screening_raw_value,
-    mammography_screening_aian,
-    mammography_screening_asian_pacific_islander,
-    mammography_screening_black,
-    mammography_screening_hispanic,
-    mammography_screening_white,
     uninsured_raw_value,
     uninsured_adults_raw_value,
     severe_housing_problems_raw_value,
@@ -53,11 +38,6 @@ clean_names_national_data_subset <- clean_names_national_data |>
     some_college_raw_value,
     high_school_completion_raw_value,
     children_in_poverty_raw_value,
-    children_in_poverty_aian,
-    children_in_poverty_asian_pacific_islander,
-    children_in_poverty_black,
-    children_in_poverty_hispanic,
-    children_in_poverty_white,
     child_care_cost_burden_raw_value,
     child_care_centers_raw_value,
     frequent_physical_distress_raw_value,
@@ -66,13 +46,6 @@ clean_names_national_data_subset <- clean_names_national_data |>
     food_insecurity_raw_value,
     insufficient_sleep_raw_value,
     teen_births_raw_value,
-    teen_births_aian,
-    teen_births_asian,
-    teen_births_black,
-    teen_births_hispanic,
-    teen_births_white,
-    teen_births_nhopi,
-    teen_births_two_or_more_races,
     excessive_drinking_raw_value,
     adult_smoking_raw_value,
     adult_obesity_raw_value,
@@ -88,17 +61,7 @@ clean_names_national_data_subset <- clean_names_national_data |>
     census_participation_raw_value,
     high_school_graduation_raw_value,
     reading_scores_raw_value,
-    reading_scores_aian,
-    reading_scores_asian_pacific_islander,
-    reading_scores_black,
-    reading_scores_hispanic,
-    reading_scores_white,
     math_scores_raw_value,
-    math_scores_aian,
-    math_scores_asian_pacific_islander,
-    math_scores_black,
-    math_scores_hispanic,
-    math_scores_white,
     living_wage_raw_value,
     residential_segregation_black_white_raw_value,
     disconnected_youth_raw_value,
@@ -138,7 +101,6 @@ clean_names_national_subset_counties[cols_to_convert] <- lapply(
   }
 )
 
-clean_names_national_subset_counties[is.na(clean_names_national_subset_counties)] <- 0
 
 options(tigris_use_cache = TRUE)
 
@@ -160,7 +122,7 @@ pal <- colorNumeric(
   na.color = "gray90"
 )
 
-m <- leaflet(map_data) |> 
+leaflet(map_data) |> 
   addProviderTiles("CartoDB.Positron") |>  
   addPolygons(
     fillColor = ~pal(preventable_hospital_stays_raw_value),
@@ -185,3 +147,8 @@ m <- leaflet(map_data) |>
     title = "Preventable Hospital Stays",
     position = "bottomright"
   )
+
+
+clean_names_national_subset_counties |> 
+  select(name, preventable_hospital_stays_raw_value, fips, state_abbreviation, population_raw_value) |> 
+  view()
